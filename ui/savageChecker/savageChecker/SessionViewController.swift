bdcaed0310f7656627324d3176858d8b7ec2e6e4
smhooper <samhooperstudio@gmail.com>
2018-05-15 13:41:53 -0800

    make default time appear when datetime fields initially tapped

--- ui/savageChecker/savageChecker/SessionViewController.swift
+++ ui/savageChecker/savageChecker/SessionViewController.swift
@@ -17,13 +17,8 @@ class SessionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var openTimeTextField: UITextField!
     @IBOutlet weak var closeTimeTextField: UITextField!
     @IBOutlet weak var viewVehiclesButton: UIBarButtonItem!
-    
     // This value is either passed by `ObservationTableViewController` in `prepare(for:sender:)` or constructed when a new session begins.
     var session: Session?
-
-    
-    // Either a session has already started and user is returning to edit or this is a new session
-    //var session = Session?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,18 +28,23 @@ class SessionViewController: UIViewController, UITextFieldDelegate {
         openTimeTextField.delegate = self
         closeTimeTextField.delegate = self
         
-        // date defaults to today
-        let now = Date()
-        var formatter = DateFormatter()
-        formatter.dateStyle = .short
-        dateTextField.text = formatter.string(from: now)
-        
         // The session has already started
         if let session = session {
             observerTextField.text = session.observerName
+            dateTextField.text = session.date
             openTimeTextField.text = session.openTime
             closeTimeTextField.text = session.closeTime
+            viewVehiclesButton.isEnabled = true // Returning to view so make sure it's enabled
             
+        }  else {
+            // date defaults to today
+            let now = Date()
+            let formatter = DateFormatter()
+            formatter.dateStyle = .short
+            dateTextField.text = formatter.string(from: now)
+            
+            // Disable navigation to vehicle list until all fields are filled
+            viewVehiclesButton.isEnabled = false
         }
         
         createOpenDatePicker()//textField: openTimeTextField)
@@ -112,6 +112,14 @@ class SessionViewController: UIViewController, UITextFieldDelegate {
         datePickerView.datePickerMode = UIDatePickerMode.time
         sender.inputView = datePickerView
         datePickerView.addTarget(self, action: #selector(handleOpenDatePicker), for: UIControlEvents.valueChanged)
+        // Set the default time to now
+        if (openTimeTextField.text?.isEmpty)! {
+            let now = Date()
+            let formatter = DateFormatter()
+            formatter.dateStyle = .none
+            formatter.timeStyle = .short
+            openTimeTextField.text = formatter.string(from: now)
+        }
     }
     
     @objc func handleOpenDatePicker(sender: UIDatePicker) {
@@ -148,6 +156,13 @@ class SessionViewController: UIViewController, UITextFieldDelegate {
         datePickerView.datePickerMode = UIDatePickerMode.time
         sender.inputView = datePickerView
         datePickerView.addTarget(self, action: #selector(handleCloseDatePicker), for: UIControlEvents.valueChanged)
+        if (closeTimeTextField.text?.isEmpty)! {
+            let now = Date()
+            let formatter = DateFormatter()
+            formatter.dateStyle = .none
+            formatter.timeStyle = .short
+            closeTimeTextField.text = formatter.string(from: now)
+        }
     }
     
     @objc func handleCloseDatePicker(sender: UIDatePicker) {
@@ -155,8 +170,8 @@ class SessionViewController: UIViewController, UITextFieldDelegate {
         timeFormatter.dateStyle = .none
         timeFormatter.timeStyle = .short
         closeTimeTextField.text = timeFormatter.string(from: sender.date)
-        print("")
     }
+    
     // Make a tool bar for the date picker with an ok button and a done button
     func createCloseDatePicker(){//textField: UITextField) {
         
@@ -167,8 +182,7 @@ class SessionViewController: UIViewController, UITextFieldDelegate {
         let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
         toolBar.setItems([flexSpace, doneButton, flexSpace], animated: true)
         
-        // Make sure this is added to the controller when openTimeTextFieldEditing is called
-        //openTimeTextField.inputAccessoryView = toolBar
+        // Make sure this is added to the controller when textFieldEditing is called
         closeTimeTextField.inputAccessoryView = toolBar
     }
     
@@ -209,6 +223,7 @@ class SessionViewController: UIViewController, UITextFieldDelegate {
         if !observerName.isEmpty && !openTime.isEmpty && !closeTime.isEmpty{
             self.session = Session(observerName: observerName, openTime: openTime, closeTime: closeTime, givenDate: date)
             print("Session updated")
+            viewVehiclesButton.isEnabled = true
         }
         
     }
