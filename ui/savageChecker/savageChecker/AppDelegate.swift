8722b46545d8f79d4f535b578feb767a8712a93b
smhooper <samhooperstudio@gmail.com>
2018-06-06 17:17:14 -0800

    add busobs table; put DB configuration in it's own function

--- ui/savageChecker/savageChecker/AppDelegate.swift
+++ ui/savageChecker/savageChecker/AppDelegate.swift
@@ -20,7 +20,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Connect to DB and create all necessary tables
+        
+        configureDatabase()
+        
+        return true
+    }
+    
+    // Connect to DB and create all necessary tables
+    func configureDatabase(){
+        // Open a connection to the database
         let db : Connection
         do {
             db = try Connection(dbPath)
@@ -28,19 +36,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             fatalError(error.localizedDescription)
         }
         print(dbPath)
-        /*let db: SQLiteDatabase
-        do {
-            db = try SQLiteDatabase.open(path: SQLiteDatabase.path)
-            print("Successfully opened connection to database.")
-        } catch SQLiteError.OpenDatabase(let message) {
-            fatalError("Unable to establish database connection")
-        } catch let error {
-            fatalError(error.localizedDescription)
-        }*/
-
+        
         // Make tables
         
-        // Session table
+        // MARK: - Session table
         let idColumn = Expression<Int64>("id")
         let observerNameColumn = Expression<String>("observerName")
         let dateColumn = Expression<String>("date")
@@ -60,12 +59,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             fatalError(error.localizedDescription)
         }
         
-        // Observations table
+        // MARK: - Observations table
         let timeColumn = Expression<String>("time")
         let driverNameColumn = Expression<String>("driverName")
         let destinationColumn = Expression<String>("destination")
         let nPassengersColumn = Expression<String>("nPassengers")
         let commentsColumn = Expression<String>("comments")
+        
         let observationsTable = Table("observations")
         do {
             try db.run(observationsTable.create(ifNotExists: true) { t in
@@ -82,35 +82,84 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             fatalError(error.localizedDescription)
         }
         
+        // MARK: - Buses table
+        let busTypeColumn = Expression<String>("busType")
+        let busNumberColumn = Expression<String>("busNumber")
+        let isTrainingColumn = Expression<Bool>("isTraining")
+        let nOvernightPassengersColumn = Expression<String>("nOvernightPassengers")
         
-        return true
+        let busesTable = Table("buses")
+        do {
+            try db.run(busesTable.create(ifNotExists: true) { t in
+                t.column(idColumn, primaryKey: .autoincrement)
+                t.column(observerNameColumn)
+                t.column(dateColumn)
+                t.column(timeColumn)
+                t.column(driverNameColumn)
+                t.column(destinationColumn)
+                t.column(nPassengersColumn)
+                t.column(commentsColumn)
+                t.column(busTypeColumn)
+                t.column(busNumberColumn)
+                t.column(isTrainingColumn)
+                t.column(nOvernightPassengersColumn)
+            })
+        } catch let error {
+            fatalError(error.localizedDescription)
+        }
+        
+        /*// MARK: - NPS vehicle table
+        let tripPurposeColumn = Expression<String>("tripPurpose")
+        let workDivisionColumn = Expression<String>("workDivision")
+        let workGroupColumn = Expression<String>("workGroup")
+        let nExpectedDaysColumn = Expression<String>("nExpectedDays")
+        
+        let NPSVehicleTable = Table("buses")
+        do {
+            try db.run(NPSVehicleTable.create(ifNotExists: true) { t in
+                t.column(idColumn, primaryKey: .autoincrement)
+                t.column(observerNameColumn)
+                t.column(dateColumn)
+                t.column(timeColumn)
+                t.column(driverNameColumn)
+                t.column(destinationColumn)
+                t.column(nPassengersColumn)
+                t.column(commentsColumn)
+                t.column(tripPurposeColumn)
+                t.column(workDivisionColumn)
+                t.column(workGroupColumn)
+                t.column(nExpectedDaysColumn)
+            })
+        } catch let error {
+            fatalError(error.localizedDescription)
+        }*/
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        print("applicationWillResignActive")
+        //print("applicationWillResignActive")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        print("applicationDidEnterBackground")
+        //print("applicationDidEnterBackground")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        print("applicationWillEnterForeground")
+        //print("applicationWillEnterForeground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        print("applicationDidBecomeActive")
+        //print("applicationDidBecomeActive")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        print("applicationWillTerminate")
+        //print("applicationWillTerminate")
     }
 
 
