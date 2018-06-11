19bfc5865e54277cb8ec9552914092109b42163d
smhooper <samhooperstudio@gmail.com>
2018-06-10 18:40:40 -0800

    add icons

--- ui/savageChecker/savageChecker/ObservationTableViewCell.swift
+++ ui/savageChecker/savageChecker/ObservationTableViewCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class ObservationTableViewCell: UITableViewCell {
     
     //MARK: Properties
-    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var mainIcon: UIImageView!
     @IBOutlet weak var driverLabel: UILabel!
     @IBOutlet weak var destinationLabel: UILabel!
     @IBOutlet weak var datetimeLabel: UILabel!
@@ -31,69 +31,105 @@ class ObservationTableViewCell: UITableViewCell {
 class BaseObservationTableViewCell: UITableViewCell {
     
     
-    let iconImage = UIImageView()
+    let mainIcon = UIImageView()
     let driverLabel = UILabel()
+    let driverIcon = UIImageView()
     let destinationLabel = UILabel()
+    let destinationIcon = UIImageView()
     let datetimeLabel = UILabel()
+    let datetimeIcon = UIImageView()
+    let nPassengersLabel = UILabel()
+    let nPassengersIcon = UIImageView()
     
-    let spacing: CGFloat = 6.0
+    let spacing: CGFloat = 8
     let largeTextSize: CGFloat = 24
     let smallTextSize: CGFloat = 17
     let mainIconImageSize: CGFloat = 90
+    let smallIconImageSize: CGFloat = 30
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        //iconImage.backgroundColor = UIColor.blue
-        
         let bundle = Bundle(for: type(of: self))
-        iconImage.image = UIImage(named: "shuttleBusImg", in: bundle, compatibleWith: self.traitCollection)
-        //iconImage.frame = CGRect(x: 0, y:0, width: self.mainIconImageSize, height: self.mainIconImageSize)
-        iconImage.contentMode = .scaleAspectFit
+        mainIcon.image = UIImage(named: "busIcon", in: bundle, compatibleWith: self.traitCollection)
+        mainIcon.frame = CGRect(x: 0, y: 0, width: mainIconImageSize, height: mainIconImageSize)
+        mainIcon.contentMode = .scaleAspectFit
         
-        contentView.addSubview(iconImage)
+        driverIcon.image = UIImage(named: "driverIcon", in: bundle, compatibleWith: self.traitCollection)
+        //driverIcon.frame = CGRect(x: 0, y: 0, width: smallIconImageSize, height: smallIconImageSize)
+        driverIcon.contentMode = .scaleAspectFit
+        
+        destinationIcon.image = UIImage(named: "destinationIcon", in: bundle, compatibleWith: self.traitCollection)
+        destinationIcon.contentMode = .scaleAspectFit
+        
+        datetimeIcon.image = UIImage(named: "clockIcon", in: bundle, compatibleWith: self.traitCollection)
+        datetimeIcon.contentMode = .scaleAspectFit
+        
+        nPassengersIcon.image = UIImage(named: "passengerIcon", in: bundle, compatibleWith: self.traitCollection)
+        nPassengersIcon.contentMode = .scaleAspectFit
+
+        contentView.addSubview(mainIcon)
         contentView.addSubview(driverLabel)
+        contentView.addSubview(driverIcon)
         contentView.addSubview(destinationLabel)
+        contentView.addSubview(destinationIcon)
         contentView.addSubview(datetimeLabel)
+        contentView.addSubview(datetimeIcon)
+        contentView.addSubview(nPassengersLabel)
+        contentView.addSubview(nPassengersIcon)
         
         // Set up constraints
-        iconImage.translatesAutoresizingMaskIntoConstraints = false
+        mainIcon.translatesAutoresizingMaskIntoConstraints = false
         driverLabel.translatesAutoresizingMaskIntoConstraints = false
+        driverIcon.translatesAutoresizingMaskIntoConstraints = false
         destinationLabel.translatesAutoresizingMaskIntoConstraints = false
+        destinationIcon.translatesAutoresizingMaskIntoConstraints = false
         datetimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        datetimeIcon.translatesAutoresizingMaskIntoConstraints = false
+        nPassengersLabel.translatesAutoresizingMaskIntoConstraints = false
+        nPassengersIcon.translatesAutoresizingMaskIntoConstraints = false
         
-        iconImage.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
-        iconImage.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        iconImage.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+        mainIcon.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        mainIcon.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        mainIcon.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+        mainIcon.widthAnchor.constraint(equalToConstant: 90)
         
+        driverIcon.heightAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        driverIcon.widthAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        driverIcon.leftAnchor.constraint(equalTo: mainIcon.rightAnchor).isActive = true
+        driverIcon.topAnchor.constraint(equalTo: mainIcon.topAnchor, constant: spacing).isActive = true
+        driverLabel.centerYAnchor.constraint(equalTo: driverIcon.centerYAnchor).isActive = true
+        driverLabel.leftAnchor.constraint(equalTo: driverIcon.rightAnchor, constant: spacing * 0.75).isActive = true
         driverLabel.textAlignment = .left
-        driverLabel.font = UIFont.systemFont(ofSize: largeTextSize)
-        driverLabel.leftAnchor.constraint(equalTo: iconImage.rightAnchor, constant: spacing).isActive = true
-        driverLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: spacing).isActive = true
+        driverLabel.font = UIFont.systemFont(ofSize: smallTextSize)
         
+        destinationIcon.heightAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        destinationIcon.widthAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        destinationIcon.centerXAnchor.constraint(equalTo: driverIcon.centerXAnchor).isActive = true
+        destinationIcon.bottomAnchor.constraint(equalTo: mainIcon.bottomAnchor, constant: spacing * -1).isActive = true
+        destinationLabel.centerYAnchor.constraint(equalTo: destinationIcon.centerYAnchor).isActive = true
+        destinationLabel.leftAnchor.constraint(equalTo: driverLabel.leftAnchor).isActive = true
         destinationLabel.textAlignment = .left
         destinationLabel.font = UIFont.systemFont(ofSize: smallTextSize)
-        destinationLabel.leftAnchor.constraint(equalTo: iconImage.rightAnchor, constant: spacing).isActive = true
-        destinationLabel.topAnchor.constraint(equalTo: driverLabel.bottomAnchor, constant: spacing).isActive = true
         
+        
+        datetimeIcon.heightAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        datetimeIcon.widthAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        datetimeIcon.leftAnchor.constraint(equalTo: driverLabel.leftAnchor, constant: contentView.frame.width/2).isActive = true
+        datetimeIcon.centerYAnchor.constraint(equalTo: driverIcon.centerYAnchor).isActive = true
+        datetimeLabel.leftAnchor.constraint(equalTo: datetimeIcon.rightAnchor, constant: spacing * 0.75).isActive = true
+        datetimeLabel.centerYAnchor.constraint(equalTo: driverIcon.centerYAnchor).isActive = true
         datetimeLabel.textAlignment = .left
         datetimeLabel.font = UIFont.systemFont(ofSize: smallTextSize)
-        datetimeLabel.leftAnchor.constraint(equalTo: iconImage.rightAnchor, constant: spacing).isActive = true
-        datetimeLabel.topAnchor.constraint(equalTo: destinationLabel.bottomAnchor, constant: spacing).isActive = true
         
-        
-        /*let viewsDict = [
-            "iconImage" : iconImage,
-            "driver" : driverLabel,
-            "destination" : destinationLabel,
-            "datetimeLabel" : datetimeLabel,
-            ] as [String : Any]
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[iconImage(10)]", options: [], metrics: nil, views: viewsDict))
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[datetimeLabel]-|", options: [], metrics: nil, views: viewsDict))
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[driver]-[destination]-|", options: [], metrics: nil, views: viewsDict))
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[driver]-[iconImage(10)]-|", options: [], metrics: nil, views: viewsDict))
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[destination]-[datetimeLabel]-|", options: [], metrics: nil, views: viewsDict))*/
+        nPassengersIcon.heightAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        nPassengersIcon.widthAnchor.constraint(equalToConstant: smallIconImageSize).isActive = true
+        nPassengersIcon.centerXAnchor.constraint(equalTo: datetimeIcon.centerXAnchor).isActive = true
+        nPassengersIcon.bottomAnchor.constraint(equalTo: destinationIcon.bottomAnchor).isActive = true
+        nPassengersLabel.centerYAnchor.constraint(equalTo: destinationIcon.centerYAnchor).isActive = true
+        nPassengersLabel.leftAnchor.constraint(equalTo: datetimeLabel.leftAnchor).isActive = true
+        nPassengersLabel.textAlignment = .left
+        nPassengersLabel.font = UIFont.systemFont(ofSize: smallTextSize)
     }
     
     required init?(coder aDecoder: NSCoder) {
