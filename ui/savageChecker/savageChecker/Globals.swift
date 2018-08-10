f779fc8287c46f821b87421c69823125f9d1a9ad
smhooper <samhooperstudio@gmail.com>
2018-08-09 16:16:37 -0800

    make swift file for mutable global vars

---
+++ ui/savageChecker/savageChecker/Globals.swift
@@ -0,0 +1,12 @@
+//
+//  Globals.swift
+//  savageChecker
+//
+//  Created by Sam Hooper on 8/9/18.
+//  Copyright © 2018 Sam Hooper. All rights reserved.
+//
+
+import SQLite
+
+
+var dbPath = "\(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!)/savageChecker.db"
