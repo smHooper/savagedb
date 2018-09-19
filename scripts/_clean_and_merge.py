69805af249427754ddbb6477ae395eea8add9c94
smhooper <samhooperstudio@gmail.com>
2018-09-19 08:16:18 -0800

    add import to db steps

--- scripts/_clean_and_merge.py
+++ scripts/_clean_and_merge.py
@@ -4,6 +4,8 @@ import accessdb_to_csv
 import _clean_tables_premerge
 import merge_year_csvs
 import _clean_tables_postmerge
+import csvs_to_postgres
+import _clean_db_after_import
 
 
 ROOT_DIR = r'C:\Users\shooper\proj\savagedb\db'
@@ -22,6 +24,13 @@ def main():
 
     # Do the rest of the stuff that has to happen post-merge
     _clean_tables_postmerge.main(os.path.join(merged_dir, 'cleaned'))
+
+    # Import csvs to DB
+    connection_txt = os.path.join(os.path.join(ROOT_DIR, '..'), 'connection_info.txt')
+    csvs_to_postgres.main(os.path.join(os.path.join(merged_dir, 'cleaned')), connection_txt=connection_txt)
+
+    # Clean up datatypes in DB
+    _clean_db_after_import.main()
 
 
 if __name__ == '__main__':
