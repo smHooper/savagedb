85ed13bddf027d855039626bf27f3b32b328c8a7
smhooper <samhooperstudio@gmail.com>
2018-10-11 15:50:05 -0800

    add 'IF EXISTS' to ALTER TABLE stmts

--- scripts/db_mgmt/_clean_db_after_import.py
+++ scripts/db_mgmt/_clean_db_after_import.py
@@ -1,7 +1,5 @@
 import sys, os
 from sqlalchemy import create_engine
-from sqlalchemy import types as sqltypes
-import pandas as pd
 
 
 INTEGER_FIELDS = {'buses':              ['n_passengers', 'n_wheelchair', 'n_lodge_ovrnt'],
@@ -13,7 +11,7 @@ INTEGER_FIELDS = {'buses':              ['n_passengers', 'n_wheelchair', 'n_lodg
                   'nps_contractors':    ['n_nights', 'n_passengers'],
                   'nps_vehicles':       ['n_nights', 'n_passengers'],
                   'photographers':      ['n_nights', 'n_passengers', 'permit_number'],
-                  'right_of_way':       ['n_passengers', 'permit_number'],
+                  'inholders':           ['n_passengers', 'permit_number'],
                   'road_lottery':       ['n_passengers', 'permit_number'],
                   'subsistence':        ['n_passengers'],
                   'tek_campers':       ['n_passengers'],
@@ -24,7 +22,7 @@ TEXT_FIELDS = {'employee_vehicles':  ['driver_name'],
                'nps_contractors':    ['organization', 'trip_purpose'],
                'nps_vehicles':       ['driver_name', 'trip_purpose', 'work_division', 'work_group'],
                'photographers':      ['driver_name'],
-               'right_of_way':       ['driver_name', 'permit_holder'],
+               'inholders':          ['driver_name', 'permit_holder'],
                'subsistence':        ['driver_name']
                }
 
@@ -44,24 +42,24 @@ def main(info_txt):
             param_name, param_value = line.split(';')
             connection_info[param_name.strip()] = param_value.strip()
 
-    for param in ['password', 'ip_address']:
+    for param in ['username', 'password', 'ip_address', 'port']:
         if param not in connection_info:
             raise NameError("missing param '%s' in connection info_txt: %s" % (param, info_txt))
 
-    engine = create_engine('postgresql://postgres:{password}@{ip_address}:5432/savage'.format(**connection_info))
+    engine = create_engine('postgresql://{username}:{password}@{ip_address}:{port}/savage'.format(**connection_info))
 
     sql = ''  # Create an empty string
     # Add statements to alter all fields that should be integers
     for table, fields in INTEGER_FIELDS.iteritems():
         for field in fields:
-            sql += 'ALTER TABLE {table_name}' \
+            sql += 'ALTER TABLE {table_name} IF EXISTS' \
                    ' ALTER COLUMN {field} SET DATA TYPE bigint' \
                    ' USING {field}::bigint; '.format(table_name=table, field=field)
 
     # Add statements to alter all fields that should be text (string)
     for table, fields in TEXT_FIELDS.iteritems():
         for field in fields:
-            sql += 'ALTER TABLE {table_name}' \
+            sql += 'ALTER TABLE {table_name} IF EXISTS' \
                    ' ALTER COLUMN {field} SET DATA TYPE text' \
                    ' USING {field}::text; '.format(table_name=table, field=field)
 
@@ -70,14 +68,14 @@ def main(info_txt):
         print 'Submitting the following commands: \n%s\n' % sql.replace('; ', ';\n')
         conn.execute(sql)
 
-        # Set all numeric fields in right_of_way_allotments to int
-        print 'Setting data types for integer fields in right_of_way_allotments table\n'
-        result = conn.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'right_of_way_allotments';")
+        # Set all numeric fields in inholder_allotments to int
+        print 'Setting data types for integer fields inholder_allotments table\n'
+        result = conn.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'inholder_allotments';")
 
         for row in result:
             field = row['column_name']
             if field != 'permit_holder':
-                conn.execute('ALTER TABLE right_of_way_allotments'
+                conn.execute('ALTER TABLE inholder_allotments'
                              ' ALTER COLUMN {field} SET DATA TYPE bigint'
                              ' USING {field}::bigint;'.format(field=field))
 
