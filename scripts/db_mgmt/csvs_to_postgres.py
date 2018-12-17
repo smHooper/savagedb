7f36570d6ba274d3656ab218d9d8a96ff5158e5f
smhooper <samhooperstudio@gmail.com>
2018-12-17 09:37:57 -0900

    use either string, csv string, or dict for primary keys

--- scripts/db_mgmt/csvs_to_postgres.py
+++ scripts/db_mgmt/csvs_to_postgres.py
@@ -26,7 +26,9 @@ import sys
 import os
 import docopt
 import re
+import warnings
 from glob import glob
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import types as sqltypes
 import pandas as pd
@@ -39,7 +41,7 @@ DTYPES = {np.int64: sqltypes.BigInteger,
           object: sqltypes.String,
           }
 
-def main(search_dir, db_name=None, password=None, username='postgres', ip_address='localhost', port=5432, connection_txt=None, primary_key='id', convert_datetimes=True):
+def main(search_dir, db_name=None, password=None, username='postgres', ip_address='localhost', port=5432, connection_txt=None, primary_key='id', foreign_key=None, convert_datetimes=True):
 
     files = glob(os.path.join(search_dir, '*.csv'))
     if len(files) == 0:
@@ -68,6 +70,17 @@ def main(search_dir, db_name=None, password=None, username='postgres', ip_addres
         message = '\n' + '\n\t'.join(['%s: %s' % (k, v) for k, v in connection_info.iteritems()])
         raise ValueError('could not establish connection with parameters:%s' % message)
 
+    if type(primary_key) == str:
+        if ',' in primary_key:
+            try:
+                primary_key = {k.strip(): v.strip() for k, v in [item.strip().split(':') for item in primary_key.split(',')]}
+            except:
+                raise ValueError('primary_key format not understood: %s. It must be either a single string or a '
+                                 'comma-separated string where each item is table_name: primary_key_col' % primary_key)
+    elif not type(primary_key) == dict:
+        raise ValueError('primary_key must either be string or dict. %s given instead' % type(primary_key))
+
+    tables = []
     for csv in files:
         basename = os.path.basename(csv)
         print 'Importing %s...' % basename
@@ -75,26 +88,59 @@ def main(search_dir, db_name=None, password=None, username='postgres', ip_addres
         table_name = basename.replace('.csv', '')
         with engine.connect() as conn, conn.begin():
             df.to_sql(table_name, conn, if_exists='append')
-            if primary_key not in df.columns:
-                sql = 'ALTER TABLE {table_name} ADD COLUMN {primary_key} SERIAL PRIMARY KEY'.format(
-                    table_name=table_name, primary_key=primary_key)
-                conn.execute(sql)
 
+            # If a dictionary of primary keys was given, try to get the key for this table. If it doesn't exists,
+            #   just us 'id'
+            if hasattr(primary_key, 'keys'):
+                if table_name in primary_key:
+                    this_primary_key = primary_key[table_name]
+                else:
+                    this_primary_key = 'id'
+            # If it isn't a dictionary, use the string given
+            else:
+                this_primary_key = primary_key
+
+           # If this primary key already exists, try to add the pkey constraint to the given column
+            if this_primary_key in df.columns:
+                sql = ''
+                # If the pkey is 'id', drop the column so it can be autoincrementing
+                if this_primary_key == 'id':
+                    sql = "ALTER TABLE {table_name} DROP COLUMN id; " \
+                          "ALTER TABLE {table_name} ADD COLUMN id SERIAL PRIMARY KEY"\
+                            .format(table_name=table_name)
+                # Otherwise, don't make the pkey auto-incrementing, but create an auto-incrementing 'id' field that isn't
+                #   a primary key
+                else:
+                    sql += 'ALTER TABLE {table_name} ADD PRIMARY KEY ({primary_key});' \
+                           'ALTER TABLE {table_name} ADD COLUMN id SERIAL;'\
+                        .format(table_name=table_name, primary_key=this_primary_key)
+            # If it doesn't exist, make it auto-incrementing
+            else:
+                sql = 'ALTER TABLE {table_name} ADD COLUMN {primary_key} SERIAL PRIMARY KEY'.format(
+                    table_name=table_name, primary_key=this_primary_key)
+            try:
+                conn.execute(sql)
+            except sqlalchemy.exc.SQLAlchemyError as e:
+                warnings.warn("Error setting primary key {primary_key} in table {table_name} because {message}"
+                              .format(primary_key=this_primary_key, table_name=table_name, message=e.message))
             if convert_datetimes:
-                sql = ''#'ALTER TABLE %s' % table_name
+                sql = ''
                 for date_col in [c for c in df.columns if c.endswith('date')]:
                     sql += 'ALTER TABLE {table_name}' \
                           ' ALTER COLUMN {date_col} SET DATA TYPE date' \
                           ' USING {date_col}::date; '.format(table_name=table_name, date_col=date_col)
                 for time_col in [c for c in df.columns if c.endswith('time')]:
                     sql += 'ALTER TABLE {table_name}' \
-                          ' ALTER COLUMN {time_col} SET DATA TYPE time' \
-                          ' USING {time_col}::time; '.format(table_name=table_name, time_col=time_col)
+                          ' ALTER COLUMN {time_col} SET DATA TYPE timestamp' \
+                          ' USING {time_col}::timestamp; '.format(table_name=table_name, time_col=time_col)
                 if sql: # will be an empty string if no tables are datetimes
                     conn.execute(sql)
+            tables.append(table_name)
 
-            # Drop the "index" column that gets automatically created when you add the primary key index
-            sql = "ALTER TABLE tt DROP COLUMN IF EXISTS {table_name};".format(table_name)
+    for table_name in tables:
+        # Drop the "index" column that gets automatically created when you add the primary key index
+        sql = "ALTER TABLE {table_name} DROP COLUMN IF EXISTS index;".format(table_name=table_name)
+        with engine.connect() as conn, conn.begin():
             conn.execute(sql)
 
 
